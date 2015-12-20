7be6ee852666e5dde93914079f829be0d0e2ba1e
Diego Souza <diegosouza@ejcm.com.br>
2015-12-20 18:39:15 -0200

    +mailtrap +-spam

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -1,16 +1,28 @@
-﻿Prazer em revê-lo, Admin.
-<br><br><br>
-Um cliente deixou uma nova mensagem em sua página de contatos:
-<br><br><br>
+﻿<!doctype html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Email</title>
+</head>
+<body>
+
+    <br>
+    Prazer em revê-lo, Admin.
+    <br><br>
+    Um cliente deixou uma nova mensagem em sua página de contatos:
+    <br><br><br>
 
 
-<p>Nome: {{$Nome}}</p>
-<p>Telefone: {{$Telefone}}</p>
-<p>E-mail: {{$Email}}</p>
-<p>Mensagem: {{$Mensagem}}</p>
+    <p>Nome: {{$Nome}}</p>
+    <p>Telefone: {{$Telefone}}</p>
+    <p>E-mail: {{$Email}}</p>
+    <p>Mensagem: {{$Mensagem}}</p>
 
-<br><br><br>
----Fim da mensagem---
-<br><br>
-Atenciosamente,
-DOGE - Desesvolvedor Orgânico da Galera dos E-mails
+    <br><br><br>
+    ---Fim da mensagem---
+    <br><br>
+    Atenciosamente,
+    DOGE - Desesvolvedor Orgânico da Galera dos E-mails
+
+</body>
+</html>
